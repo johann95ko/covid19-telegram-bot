e9f48469379d3b6a4280b05361af36535c26e14e
unknown <johann95ko@hotmail.com>
2020-04-17 16:03:39 +0800

    testing another format of markdown argument input

--- telegram_bot.py
+++ telegram_bot.py
@@ -146,7 +146,7 @@ class TelegramBot:
         Sends message to Telegram servers.
         """
 
-        res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(self.chat_id, self.outgoing_message_text, 'telegram.ParseMode.MARKDOWN_V2'))
+        res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(self.chat_id, self.outgoing_message_text))
 
         return True if res.status_code == 200 else False
     
