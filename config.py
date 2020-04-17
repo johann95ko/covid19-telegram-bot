2a58ffba9377025a2e323db492c5853aa391c442
unknown <johann95ko@hotmail.com>
2020-04-17 16:07:49 +0800

    test 2

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ SERVER_URL = 'https://covid-sg-telegram-bot.herokuapp.com/'
 BASE_TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
 LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(SERVER_URL)
 TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
-TELEGRAM_SEND_MESSAGE_URL = BASE_TELEGRAM_URL + '/sendMessage?chat_id={}&text={}&parse_mode=telegram.ParseMode.MARKDOWN_V2'
+TELEGRAM_SEND_MESSAGE_URL = BASE_TELEGRAM_URL + '/sendMessage?chat_id={}&text={}&parse_mode=Markdown'
